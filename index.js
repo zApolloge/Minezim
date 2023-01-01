6cebe4d82a3896cdc2758e2457cf3aa92a85eec4
Mayam <121643450+zApolloge@users.noreply.github.com>
2023-01-01 19:45:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function lobby(){
+    window.location.href = 'https://discord.gg/pvqMSBkEGg';
+}
+
+function games(){
+  window.location.href = 'https://kaiollzim.github.io/minezim/'
+}
+
+function colorbackground(){
+  document.body.style.background = ' linear-gradient(90deg, #6818b5 0%, #000000 100%)'
+  main.style.background = '#bba2ba'
+  dark.style.background = '#bba2ba'
+  discserver.style.background = '#bba2ba'
+  light.style.background = '#bba2ba'
+}
+
+function li(){
+  document.body.style.background = '#1a202c'
+  main.style.background = 'rgb(45, 55, 72)'
+  dark.style.background = 'rgb(45, 55, 72)'
+  discserver.style.background = 'rgb(45, 55, 72)'
+  light.style.background = 'rgb(45, 55, 72)'
+}
